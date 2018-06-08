5105beac84c59c0545d5f3b102f39e78fff9ebc2
RedLeader12 <redleader@jigsaw.xyz>
2018-06-08 08:19:03 +0800

    Added logic of methods from persons container to the reducer

--- src/containers/store/reducers/reducer.js
+++ src/containers/store/reducers/reducer.js
@@ -7,13 +7,20 @@ const initialState = {
 const reducer = (state = initialState, action) => {
     switch( action.type ){
         case actionTypes.ADD_PERSON: {
+            const newPerson = {
+                id: Math.random(), // not really unique but good enough here!
+                name: 'Max',
+                age: Math.floor( Math.random() * 40 )
+            }
             return {
-                ...state
+                ...state,
+                persons: state.persons.concat(newPerson)
             }
         }
         case actionTypes.REMOVE_PERSON:
         return {
             ...state,
+            persons: state.persons.filter(person => person.id !== action.personId)
             
         }   
     }
