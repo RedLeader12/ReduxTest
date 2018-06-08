f5b3a8c7dc121db24a14b6704bdd92407699de71
RedLeader12 <redleader@jigsaw.xyz>
2018-06-08 08:24:50 +0800

    Connected redux to react in the person's container

--- src/containers/Persons.js
+++ src/containers/Persons.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import Person from '../components/Person/Person';
 import AddPerson from '../components/AddPerson/AddPerson';
+
+import * as actionTypes from '../store/actions';
 
 class Persons extends Component {
     state = {
@@ -11,17 +14,30 @@ class Persons extends Component {
     render () {
         return (
             <div>
-                <AddPerson personAdded={this.personAddedHandler} />
+                <AddPerson personAdded={this.props.onAddPerson} />
                 {this.state.persons.map(person => (
                     <Person 
                         key={person.id}
                         name={person.name} 
                         age={person.age} 
-                        clicked={() => this.personDeletedHandler(person.id)}/>
+                        clicked={() => this.props.onRemovedPerson(person.id)}/>
                 ))}
             </div>
         );
     }
 }
 
-export default Persons;
+const mapStateToProps = state => {
+    return {
+        prs: state.persons
+    }
+};
+
+const mapDispatchToProps = dispatch =>  {
+    return {
+        onAddPerson: () => dispatch({type: actionTypes.ADD_PERSON}), 
+        onRemovedPerson: (id) => dispatch({type: actionTypes.REMOVE_PERSON}, personId: id)
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Persons)
